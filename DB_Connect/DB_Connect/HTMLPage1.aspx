489755b449c5012a03511c9e60adb99e9f1041f3
Makaziwe <mamahlangu@deloitte.co.za>
2024-01-26 08:47:56 +0200

    Db Connection

---
+++ DB_Connect/DB_Connect/HTMLPage1.aspx
@@ -0,0 +1,19 @@
+﻿<!DOCTYPE html>
+
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <asp:TextBox ID="txtTableName" runat="server" />
+    <asp:RequiredFieldValidator ErrorMessage="Required" ControlToValidate="txtTableName"
+ Display ="Dynamic" runat="server" />
+<asp:Button Text="Create Table" OnClick="CreateTable" runat="server" />
+
+    </form>
+
+</body>
+</html>
+
---
+++ DB_Connect/DB_Connect/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+namespace DB_Connect
+{
+    class Program
+    {
+        // connection string
+        static string connString = "Server=levvia-resource-manager.database.windows.net;Database=levvia-resource-manager-sql2;User Id=levvia-resource-manager-sql@levvia-resource-manager;Password=&mx3x9eVwa&Uns;";
+
+        static void Main()
+
+        {
+
+            
+
+            using (SqlConnection connection = new SqlConnection(connString))
+
+            {
+
+                // Open connection
+
+                connection.Open();
+
+                Console.WriteLine("Connected successfully.");
+
+                string query = "select * from sys.tables";
+
+                using (SqlCommand cmd = new SqlCommand(query, connection))
+
+                {
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+
+                    {
+
+                        while (reader.Read())
+
+                        {
+
+                            // Process data from the reader
+
+                            Console.WriteLine(reader.GetString(0)); // Example of accessing column data
+                            cmd.ExecuteNonQuery();
+                            //close connection
+                            connection.Close();
+                        }
+
+                    }
+
+                }
+
+
+            }
+
+        }
+
+        private static void CreateTable(object sender, EventArgs e)
+        {
+
+            string sqlStatment = string.Format("CREATE TABLE {0}([Id] [int] IDENTITY(1,1) NOT NULL, [Name] [varchar](50) NULL)", "Person");
+            using (SqlConnection con = new SqlConnection(connString))
+            {
+                using (SqlCommand cmd = new SqlCommand(sqlStatment, con))
+                {
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+            }
+
+        }
+
+    }
+}
